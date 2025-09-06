1ec52d803ca95df45c1ff068aa460ae1239bf826
LM-Firefly <firefly.lzh@gmail.com>
2025-09-06 17:09:53 +0800

    Refactor proxy construction functions and add new protocols
    - Updated the signatures of existing proxy construction functions to include additional parameters such as ALPN lists and fingerprints.
    - Introduced new proxy construction functions for VLESS, TUIC, anyTLS, and mieru protocols.
    - Removed deprecated hysteria and hysteria2 construction functions.
    - Added explode functions for new protocols to handle parsing.
    - Implemented a new utility function `splitKeyValue` for splitting strings at the first occurrence of a separator, useful for parsing key-value pairs.

--- src/parser/config/proxy.h
+++ src/parser/config/proxy.h
@@ -15,6 +15,7 @@ enum class ProxyType
     Shadowsocks,
     ShadowsocksR,
     VMess,
+    VLESS,
     Trojan,
     Snell,
     HTTP,
@@ -22,7 +23,10 @@ enum class ProxyType
     SOCKS5,
     WireGuard,
     Hysteria,
-    Hysteria2
+    Hysteria2,
+    TUIC,
+    AnyTLS,
+    Mieru
 };
 
 inline String getProxyTypeName(ProxyType type)
@@ -35,6 +39,8 @@ inline String getProxyTypeName(ProxyType type)
         return "SSR";
     case ProxyType::VMess:
         return "VMess";
+    case ProxyType::VLESS:
+        return "VLESS";
     case ProxyType::Trojan:
         return "Trojan";
     case ProxyType::Snell:
@@ -51,6 +57,12 @@ inline String getProxyTypeName(ProxyType type)
         return "Hysteria";
     case ProxyType::Hysteria2:
         return "Hysteria2";
+    case ProxyType::TUIC:
+        return "TUIC";
+    case ProxyType::AnyTLS:
+        return "AnyTLS";
+    case ProxyType::Mieru:
+        return "Mieru";
     default:
         return "Unknown";
     }
@@ -84,16 +96,25 @@ struct Proxy
     String Host;
     String Path;
     String Edge;
+    StringArray H2Hosts; // h2-opts.host 数组
+    StringArray H2Paths; // h2-opts.path 数组
+    StringArray HTTPPaths; // http-opts.path 数组
+    std::map<String, StringArray> HTTPHeaders; // http-opts.headers 多值
+    StringArray GRPCServiceNames; // grpc-opts.grpc-service-name 数组
 
     String QUICSecure;
     String QUICSecret;
 
+    tribool SmuxEnabled;
     tribool UDP;
+    tribool XUDP;
     tribool TCPFastOpen;
     tribool AllowInsecure;
     tribool TLS13;
+    tribool UDPoverTCP;
 
     String UnderlyingProxy;
+    String IPVersion;
 
     uint16_t SnellVersion = 0;
     String ServerName;
@@ -115,23 +136,92 @@ struct Proxy
     uint32_t UpSpeed;
     String Down;
     uint32_t DownSpeed;
+    String Auth;
     String AuthStr;
     String SNI;
+    String OBFSPassword;
     String Fingerprint;
     String Ca;
     String CaStr;
+    String Alpn;
+    std::vector<String> AlpnList;
     uint32_t RecvWindowConn;
     uint32_t RecvWindow;
     tribool DisableMtuDiscovery;
     uint32_t HopInterval;
-    StringArray Alpn;
-
     uint32_t CWND = 0;
+
+    String UUID;
+    String IP;
+    String HeartbeatInterval;
+    tribool DisableSNI;
+    tribool ReduceRTT;
+    uint32_t RequestTimeout;
+    String UdpRelayMode;
+    String CongestionController;
+    uint32_t MaxUdpRelayPacketSize;
+    tribool FastOpen;
+    uint32_t MaxOpenStreams;
+
+    uint32_t IdleSessionCheckInterval;
+    uint32_t IdleSessionTimeout;
+    uint32_t MinIdleSession;
+
+    String Flow;
+    uint32_t XTLS;
+    String PacketEncoding;
+    String ShortID;
+
+    int SmuxMaxConnections = 0;
+    int SmuxMaxStreams = 0;
+    int SmuxMinStreams = 0;
+    tribool SmuxPadding;
+    tribool SmuxStatistic;
+    tribool SmuxOnlyTcp;
+
+    String RestlsScript;
+    String ShortId;
+    tribool Tfo;
+    tribool PortHopping;
+    String PortRange;
+    String IpWhitelist;
+    String Psk;
+    String ScMaxTime;
+    String ScMaxSize;
+    String ScMinTime;
+    String ScMinSize;
+    String ScSize;
+    String ScTime;
+    String Smux;
+    tribool UnderlyingProxyEnable;
+
+    String Network;
+    String ClientFingerprint;
+    String EchConfig;
+    tribool EchEnable;
+    tribool SupportX25519Mlkem768;
+    String GrpcServiceName;
+    String GRPCMode;
+    String WsPath;
+    String WsHeaders;
+    std::string WsEarlyDataHeaderName;
+    int WsMaxEarlyData = 0;
+    tribool V2rayHttpUpgrade;
+    tribool V2rayHttpUpgradeFastOpen;
+    
+    uint32_t InitialStreamReceiveWindow = 0;
+    uint32_t MaxStreamReceiveWindow = 0;
+    uint32_t InitialConnectionReceiveWindow = 0;
+    uint32_t MaxConnectionReceiveWindow = 0;
+
+    String Multiplexing;
+    String TLSStr;
 };
 
 #define SS_DEFAULT_GROUP "SSProvider"
 #define SSR_DEFAULT_GROUP "SSRProvider"
 #define V2RAY_DEFAULT_GROUP "V2RayProvider"
+#define VLESS_DEFAULT_GROUP "VLESSProvider"
 #define SOCKS_DEFAULT_GROUP "SocksProvider"
 #define HTTP_DEFAULT_GROUP "HTTPProvider"
 #define TROJAN_DEFAULT_GROUP "TrojanProvider"
@@ -139,5 +229,8 @@ struct Proxy
 #define WG_DEFAULT_GROUP "WireGuardProvider"
 #define HYSTERIA_DEFAULT_GROUP "HysteriaProvider"
 #define HYSTERIA2_DEFAULT_GROUP "Hysteria2Provider"
+#define TUIC_DEFAULT_GROUP "TUICProvider"
+#define ANYTLS_DEFAULT_GROUP "AnyTLSProvider"
+#define MIERU_DEFAULT_GROUP "MieruProvider"
 
 #endif // PROXY_H_INCLUDED
